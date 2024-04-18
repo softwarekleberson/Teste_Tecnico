576b9ac1f52e3cf321a853910f0031512f2b228d
softwarekleberson <santossilvakleberson@gmail.com>
2024-04-18 15:48:26 -0300

    MOdifucalçao do server, inserindo mensagem seja bem vindo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from './src/app.js'
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+    console.log(`Servidor rodando no endereço http://localhost:${PORT} seja bem vindo`)
 })
 
 
